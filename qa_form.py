459f2ae46cb50015214d25f66977019f43d43773
vyanezz <yanez.vc@gmail.com>
2026-02-25 23:22:41 +0100

    unified test object

--- qa_form.py
+++ qa_form.py
@@ -2,19 +2,17 @@ from test_automation import TestAutomation
 
 def test_qa_form():
         if __name__ == '__main__':
-                test = TestAutomation()
-                driver, traffic = test.start_test()
+                test = TestAutomation().start_test()
 
-                driver.navigate("https://ultimateqa.com/filling-out-forms/")
+                test.navigate("https://ultimateqa.com/filling-out-forms/")
 
-                assert traffic.validate_api_call("https://ultimateqa.com/filling-out-forms/", "GET")
+                assert test.validate_api_call("https://ultimateqa.com/filling-out-forms/", "GET")
 
-                driver.send_keys("id", "et_pb_contact_name_0", "Test User")
-                driver.send_keys("id", "et_pb_contact_message_0", "Pytest message")
-                driver.click("name", "et_builder_submit_button")
+                test.send_keys("id", "et_pb_contact_name_0", "Test User")
+                test.send_keys("id", "et_pb_contact_message_0", "Pytest message")
+                test.click("name", "et_builder_submit_button")
 
-                assert traffic.validate_api_call("https://ultimateqa.com/filling-out-forms/", "POST")
+                assert test.validate_api_call("https://ultimateqa.com/filling-out-forms/", "POST")
 
-                driver.quit()
-                traffic.stop()
+                test.stop_test()
 test_qa_form()
--- test_automation.py
+++ test_automation.py
@@ -10,5 +10,18 @@ class TestAutomation:
     def start_test(self):
         self.traffic.start()
         self.driver = WebDriver()
+        return self
 
-        return self.driver, self.traffic
+    def stop_test(self):
+        if self.driver:
+            self.driver.quit()
+        if self.traffic:
+            self.traffic.stop()
+
+    def __getattr__(self, name):
+        if hasattr(self.traffic, name):
+            return getattr(self.traffic, name)
+        elif hasattr(self.driver, name):
+            return getattr(self.driver, name)
+        else:
+            raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")

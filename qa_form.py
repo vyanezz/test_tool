b5a5ccf7ccfd3a2c00b65cf68e46347f71cd9274
vyanezz <yanez.vc@gmail.com>
2026-03-13 13:09:58 +0100

    Add request validation: headers, JSON body, form-urlencoded body with descriptive errors

--- qa_form.py
+++ qa_form.py
@@ -2,17 +2,28 @@ from test_automation import TestAutomation
 
 def test_qa_form():
         if __name__ == '__main__':
-                test = TestAutomation(enable_logger=False)
+                test = TestAutomation(enable_logger=True)
 
                 test.navigate("https://ultimateqa.com/filling-out-forms/")
 
-                assert test.wait_for_call("https://ultimateqa.com/filling-out-forms/", "GET")
+                test.wait_for_request("https://ultimateqa.com/filling-out-forms/", "GET")
 
                 test.send_keys("id", "et_pb_contact_name_0", "Test User")
-                test.send_keys("id", "et_pb_contact_message_0", "Pytest message")
+                test.send_keys("id", "et_pb_contact_message_0", "Test message")
                 test.click("name", "et_builder_submit_button")
 
-                assert test.wait_for_call("https://ultimateqa.com/filling-out-forms/", "POST", 10)
+                test.wait_for_request(endpoint="https://ultimateqa.com/filling-out-forms/",
+                                          method="POST",
+                                          timeout=10,
+                                          headers={"accept-language": "es-ES,es;q=0.9", "cache-control":"max-age=0"},
+                                          form_urlencoded_body={
+                                              "et_pb_contact_name_0": "Test Users",
+                                              "et_pb_contact_message_0": "Test message",
+                                              "et_pb_contactform_submit_0": "et_contact_proccess",
+                                              "_wpnonce-et-pb-contact-form-submitted-0": "bf019df153",
+                                              "_wp_http_referer": "/filling-out-forms/"
+                                          }
+                                      )
 
                 test.stop_test()
 test_qa_form()
